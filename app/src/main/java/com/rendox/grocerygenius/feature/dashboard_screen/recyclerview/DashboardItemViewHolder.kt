de924f036f19486ee0ecfca0b8519875e0b199fb
DanielRendox <daniel.rendox@gmail.com>
2024-09-15 18:29:17 +0200

    Add content descriptions to drag handle icons

--- app/src/main/java/com/rendox/grocerygenius/feature/dashboard_screen/recyclerview/DashboardItemViewHolder.kt
+++ app/src/main/java/com/rendox/grocerygenius/feature/dashboard_screen/recyclerview/DashboardItemViewHolder.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.pluralStringResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -90,7 +91,7 @@ private fun ListItem(
             Icon(
                 modifier = Modifier.padding(8.dp),
                 painter = painterResource(id = R.drawable.baseline_drag_handle_24),
-                contentDescription = null,
+                contentDescription = stringResource(id = R.string.drag_handle),
             )
         }
     }
--- app/src/main/java/com/rendox/grocerygenius/feature/settings/categories/recyclerview/CategoryViewHolder.kt
+++ app/src/main/java/com/rendox/grocerygenius/feature/settings/categories/recyclerview/CategoryViewHolder.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.recyclerview.widget.RecyclerView
@@ -71,7 +72,7 @@ private fun CategoryItem(
             Icon(
                 modifier = Modifier.padding(8.dp),
                 painter = painterResource(id = R.drawable.baseline_drag_handle_24),
-                contentDescription = null,
+                contentDescription = stringResource(id = R.string.drag_handle),
             )
         }
     }
